b0f51ac8d097c5ecce8c9a63ec961517b3aaf490
uttammittal02 <hemanthmittal02@gmail.com>
2022-03-21 21:23:05 +0530

    upload files

---
+++ os_assign/lseek_.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+void func(char arr[], int n)
+{
+    int f_write = open("./start.txt", O_RDONLY);
+    int f_read = open("./end.txt", O_WRONLY);
+    int count = 0;
+    while (read(f_write, arr, 1)) {
+            // printf(&arr[count]);
+        if (count < n) {
+            lseek (f_write, n, SEEK_CUR);
+            write (f_read, arr, 1);
+            count = n;
+        }
+        else {
+            count = (2*n);
+            lseek(f_write, count, SEEK_CUR);
+            write(f_read, arr, 1);
+        }
+    }
+    close(f_write);
+    close(f_read);
+}
+
+int main() {
+    char arr[100];
+    int n; n = 5;
+    func(arr, n);
+    return 0; 
+}
